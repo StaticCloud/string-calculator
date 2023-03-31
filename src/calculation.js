f8fd706c25cd9de56fd843316c29f54e83e6fb83
DMarrs <diegomarrs@gmail.com>
2023-03-30 18:08:43 -0700

    added extra tests for subtraction, multiplication, and division

--- src/calculation.js
+++ src/calculation.js
@@ -3,8 +3,16 @@ const math = require('mathjs')
 function calculation(input) {
     const client_defined_maxlength = 20;
 
+    // verifies if the input only contains ASCII characters
     const validASCII = input.match('^[\x00-\x7F]*$');
+
+    // verifies if the input length is less than or equal to the length specified by the client
     const validLength = (input.length <= client_defined_maxlength);
+
+    /* 
+        verifies if the user input contains numberic characters from 0-9, 
+        including negative numbers, and the characters ( ) + . *
+    */ 
     const validNumbers = input.match('^[-0-9\(\)\+\*\/\. ]*$');
 
     // check if the input contains ASCII characters and the length is less than or equal to 20
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -5,16 +5,14 @@ const calculation = require('../src/calculation')
 //     })
 // })
 
-const client_cases = [
-    ['ÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹ'],
-    ['ÁÉÍÓÚÝĆǴḰĹḾŃ'],
-    ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-]
-
 describe('CLIENT: Argument is 0-20 characters in length, is any ASCII character', () => {
-    test.each(client_cases)('Should throw an error if criteria is not met', (input) => {
-        const callback = () => calculation(input)
-        expect(callback).toThrow(new Error("Invalid input"));
+    test.each([
+            ['ÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹÁÉÍÓÚÝĆǴḰĹḾŃṔŔŚẂŹ'],
+            ['ÁÉÍÓÚÝĆǴḰĹḾŃ'],
+            ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+        ])('Should throw an error if criteria is not met', (input) => {
+            const callback = () => calculation(input)
+            expect(callback).toThrow(new Error("Invalid input"));
     })
 })
 
@@ -31,9 +29,34 @@ describe("CLIENT: Throw error if input contains non-numeric characters", () => {
     })
 })
 
+// ? CLIENT QUESTION: Can we combine the tests for addition, subtraction, multiplication, and division into one test for simplification?
 describe("CLIENT: return the sum of two numbers", () => {
     test.each([ ["2 + 2", "4"], ["5 + 6", "11"], ["-9 + 10", "1"], ["-2 + -2", "-4"]])("%s should equal %s", (equation , sum) => {
         expect(calculation(equation)).toEqual(sum)
+    })
+})
+
+describe("CLIENT: return the difference between two numbers", () => {
+    test.each([ ["2 - 2", "0"], ["-10 - -9", "-1"], ["10 - 12", "-2"], ["16 - 8", "8"] ])("%s should equal %s", (equation , difference) => {
+        expect(calculation(equation)).toEqual(difference)
+    })
+})
+
+describe("CLIENT: return the product between two numbers", () => {
+    test.each([ ["6 * 10", "60"], ["0 * 100000", "0"], ["-3 * -3", "9"], ["9 * -2", "-18"] ])("%s should equal %s", (equation , product) => {
+        expect(calculation(equation)).toEqual(product)
+    })
+})
+
+/* 
+? CLIENT QUESTION: 
+? How should we handle improper or proper fractions? Should we return 
+? the original fraction, or return a decimal number? If we do want to 
+? return a decimal number, how many decimal places should we round it to? 
+*/
+describe("CLIENT: return the quotient between two numbers", () => {
+    test.each([ ["6 / 2", "3"], ["1 / 0", "Infinity"] ])("%s should equal %s", (equation , quotient) => {
+        expect(calculation(equation)).toEqual(quotient)
     })
 })
 
